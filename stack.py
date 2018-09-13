42ac59461570de69d1f51171f925ddc1fb0b03cf
Arthur Atrokhov <aatrokhov@gmail.com>
2018-09-13 15:51:55 +0600

    Update

--- stack.py
+++ stack.py
@@ -1,22 +1,14 @@
-class Stack(object):
+class Stack:
 
     def __init__(self, length):
         self.items = []
         self.length = length
 
     def is_empty(self):
-        if len(self.items) == 0:
-            return True
-        # This 'else' need for returning False, not None
-        else:
-            return False
+        return not bool(self.items)
 
     def is_full(self):
-        if len(self.items) >= self.length:
-            return True
-        # This 'else' need for returning False, not None
-        else:
-            return False
+        return bool(len(self.items) >= self.length)
 
     def push(self, item):
         if not self.is_full():
@@ -25,15 +17,15 @@ class Stack(object):
             return "Stack is full!"
 
     def pop(self):
-        if not self.is_empty():
+        try: 
             return self.items.pop()
-        else:
+        except IndexError:
             return "Stack is empty!"
 
     def top(self):
-        if not self.is_empty():
+        try:
             return self.items[-1]
-        else:
+        except IndexError:
             return "Stack is empty!"
 
     def size(self):
